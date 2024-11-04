f5d87d4bb8902da8882fedf2f33342c614b7e604
Rafael Ribeiro <ra20fael04@gmail.com>
2024-11-04 19:01:31 -0300

    feat: create service worker.

---
+++ js/serviceWorker.js
@@ -0,0 +1,38 @@
+async function addResourcesToCache(resources) {
+  const cache = await caches.open("v1")
+  await cache.addAll(resources)
+}
+
+async function putInCache(request, response) {
+  const cache = await caches.open("v1");
+  await cache.put(request, response);
+}
+
+async function respondWithCachedContent(event) {
+  const matches = await caches.match(event.request)
+
+  console.log(matches);
+  if (matches) return matches
+
+  const responseFromNetwork = await fetch(request);
+  putInCache(request, responseFromNetwork.clone());
+  return responseFromNetwork;
+}
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    addResourcesToCache([
+      "/index.html",
+      "/js/script.js",
+      "/js/returnArrayOfPanelValues.js",
+      "/style.css",
+      "/images/bg-stars.svg",
+      "/images/download.svg",
+      "/images/pattern-hills.svg",
+    ])
+  )
+})
+
+self.addEventListener("fetch", event => {
+  event.respondWith(respondWithCachedContent(event.request))
+})
